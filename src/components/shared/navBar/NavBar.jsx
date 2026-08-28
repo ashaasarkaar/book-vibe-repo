a3e26629873cfbd2cffa078bf039c71f8c8257e0
Salaatsarkar <salaatsarkar0501@gmail.com>
2026-08-28 11:43:30 +0600

    NavBar Design Done

--- src/components/shared/navBar/NavBar.jsx
+++ src/components/shared/navBar/NavBar.jsx
@@ -1,53 +1,65 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const NavBar = () => {
-    return (
-        <div>
-            
-            <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex={-1}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+  const links = (
+    <>
+      <li><NavLink to={'/'} className={({ isActive }) =>
+        `font-semibold mr-2
+      ${isActive
+          ? 'border-2 border-[#23BE0A]'
+          : ''}`
+      }>Home</NavLink></li>
+
+      <li><NavLink className={({ isActive }) =>
+        `font-semibold  mr-2
+        ${isActive
+          ? 'border-2 border-[#23BE0A]'
+          : ''}`} to={'/books'}>Listed Books</NavLink></li>
+
+      <li><NavLink className={({ isActive }) =>
+        `font-semibold  mr-2
+      ${isActive ?
+          'border-2 border-[#23BE0A]'
+          : ''}`} to={'/pages-to-read'}>Pages to Read</NavLink></li>
+    </>
+
+
+  )
+  return (
+    <div>
+
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              {links}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Book Vibe</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {links}
           </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+        </div>
+        <div className="navbar-end">
+
+          <div className='flex gap-2'>
+            <button className='btn bg-[#23BE0A] text-white rounded-md'>Sign In</button>
+            <button className='btn bg-[#59C6D2] text-white rounded-md'>Sign Up</button>
+          </div>
 
         </div>
-    );
+      </div>
+
+    </div>
+  );
 };
 
 export default NavBar;
